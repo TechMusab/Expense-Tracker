ee071454fa6be87a39a36c12ad73b2aab9419013
Musab Bin Ubaid <joiya4636@gmail.com>
2024-07-04 17:20:54 -0700

    updated logic

--- script.js
+++ script.js
@@ -2,12 +2,22 @@ let save = document.querySelector('.save');
 const expensename = document.querySelector('.nameinput');
 const expense = document.querySelector('.expenseinput');
 const date = document.querySelector('.dateinput');
+let items=document.querySelector('.items');
+items.innerHTML="";
 let array = (JSON.parse(localStorage.getItem('array'))) || [];
+function deleteitems(){
+    let deletesbtn=document.querySelectorAll('.delete');
+    deletesbtn.forEach((button, index) => {
+        button.addEventListener("click", (e) => {
+            array.splice(index, 1); 
+            localStorage.setItem("array",JSON.stringify(array));
+            saveitems(); 
+        });
+    })
+    }
 
 save.addEventListener("click", e => {
-    console.log("haha"); 
     e.preventDefault();
-    
     let obj = {
         name: expensename.value,
         expense: expense.value,
@@ -16,6 +26,37 @@ save.addEventListener("click", e => {
     
     array.push(obj);
     localStorage.setItem("array",JSON.stringify(array));
+    saveitems();
     console.log(array);
-
 });
+
+function gethtml(name,amount,date){
+  
+    items.innerHTML+=`
+    <div class="item">
+                <h4>
+                    ${name}
+                </h4>
+                <h4>
+                    ${amount}
+                </h4>
+                <h4>
+                    ${date}
+                </h4>
+                <button class="delete">
+                    Delete
+                </button>
+            </div>
+    `;
+
+}
+function saveitems(){
+    items.innerHTML="";
+    for(let item of array){
+        gethtml(item.name,item.expense,item.date);
+    }
+    deleteitems();
+}
+saveitems();
+// Challange i have faced during this
+// 1. i attached the event listeners to all buttons. but when i delete it i recreate the whole array and when recreated the event listeners are not attached to that array. so i have to attach the event listeners again by making the function.
